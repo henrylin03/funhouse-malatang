97fea428be5c896a9d2c6022ed1f3659722d9c4e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-15 13:06:16 +1000

    add hero image

--- src/home.js
+++ src/home.js
@@ -1,18 +1,19 @@
 import hotpotImagePath from "./assets/hotpot.jpg";
 
-export function load() {
-  const element = document.createElement("div");
+export function createHomePage() {
+  const mainHTML = document.querySelector("main");
+  const heroSection = document.createElement("section");
+  heroSection.classList.add("hero");
 
-  // Lodash, now imported by this script
-  element.textContent = "this is a test";
-  element.classList.add("hello");
+  mainHTML.appendChild(heroSection);
 
-  const malatangImg = new Image();
-  malatangImg.src = hotpotImagePath;
-
-  element.appendChild(malatangImg);
-
-  console.log("hello");
-
-  document.body.appendChild(element);
+  // const element = document.createElement("div");
+  // // Lodash, now imported by this script
+  // element.textContent = "this is a test";
+  // element.classList.add("hello");
+  // const malatangImg = new Image();
+  // malatangImg.src = hotpotImagePath;
+  // element.appendChild(malatangImg);
+  // console.log("hello");
+  // document.body.appendChild(element);
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles/normalise.css";
 import "./styles/style.css";
-import { load as loadHomePage } from "./home";
+import { createHomePage } from "./home";
 
-const page = loadHomePage()();
+createHomePage();
