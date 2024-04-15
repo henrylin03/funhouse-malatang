e263ed61ad9fe907b0424730c7e43b4eafa30c13
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-15 13:22:59 +1000

    style button and text

--- src/home.js
+++ src/home.js
@@ -20,14 +20,4 @@ export function createHomePage() {
   heroInnerContainer.appendChild(ctaBtn);
   heroSection.appendChild(heroInnerContainer);
   mainElement.appendChild(heroSection);
-
-  // const element = document.createElement("div");
-  // // Lodash, now imported by this script
-  // element.textContent = "this is a test";
-  // element.classList.add("hello");
-  // const malatangImg = new Image();
-  // malatangImg.src = hotpotImagePath;
-  // element.appendChild(malatangImg);
-  // console.log("hello");
-  // document.body.appendChild(element);
 }
