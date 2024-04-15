9af9a0dcaad5f33ebb5665ae1c81562cf1f901c5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-15 14:20:06 +1000

    add copy to about section. add about section to main.

--- src/home.js
+++ src/home.js
@@ -1,8 +1,11 @@
 export function createHomePage() {
   const mainElement = document.querySelector("main");
+
   const heroSection = createHero();
+  const aboutSection = createAbout();
 
   mainElement.appendChild(heroSection);
+  mainElement.appendChild(aboutSection);
 }
 
 function createHero() {
@@ -25,4 +28,20 @@ function createHero() {
   heroSection.appendChild(heroInnerContainer);
 
   return heroSection;
+}
+
+function createAbout() {
+  const aboutSection = document.createElement("section");
+  aboutSection.classList.add("about");
+
+  const heading = document.createElement("h2");
+  heading.textContent = "About us";
+
+  const copy = document.createElement("p");
+  copy.textContent = `In 1908, a secret soup recipe in Chongqing kickstarted the beginning of Chinese hotpot as we know it. The recipe combined more than a dozen of ingredients and spices, creating the perfect hotpot broth: slightly numbing, moderately spicy but easy on the stomach.
+  
+  Fast forward to 2016 when, with the help of a 3rd generation descendant of the recipe holder, Funhouse Malatang was started in Sydney, Australia. Having expanded to three locations, Funhouse continues to share the authentic Chongqing flavours and soul-warming broths to its happy customers.`;
+
+  aboutSection.appendChild(heading);
+  aboutSection.appendChild(copy);
 }
