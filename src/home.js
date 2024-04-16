b22e5753de707b0d15fc50cfe7bad4507881e6ad
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-16 14:54:45 +1000

    replace font with open sans
    roboto seems a bit more rugged and less 'fun'

--- src/home.js
+++ src/home.js
@@ -1,18 +1,10 @@
 export function createHomePage() {
   const mainElement = document.querySelector("main");
-
-  const heroSection = createHero();
-  const aboutSection = createAbout();
-  const menuSection = createMenu();
-
-  mainElement.appendChild(heroSection);
-  // mainElement.appendChild(aboutSection);
-  mainElement.appendChild(menuSection);
 }
 
 function createHero() {
-  const heroSection = document.createElement("section");
-  heroSection.classList.add("hero");
+  const section = document.createElement("section");
+  section.classList.add("hero");
 
   const heroInnerContainer = document.createElement("div");
   heroInnerContainer.classList.add("inner");
@@ -29,34 +21,5 @@ function createHero() {
   heroInnerContainer.appendChild(ctaBtn);
   heroSection.appendChild(heroInnerContainer);
 
-  return heroSection;
-}
-
-// function createAbout() {
-//   const aboutSection = document.createElement("section");
-//   aboutSection.classList.add("about");
-
-//   const heading = document.createElement("h2");
-//   heading.textContent = "Our story";
-
-//   const copy = document.createElement("p");
-//   copy.innerHTML =
-//     "In 1908, a secret soup recipe in Chongqing kickstarted the beginning of Chinese hotpot as we know it. The recipe combined more than a dozen of ingredients and spices, creating the perfect hotpot broth: slightly numbing, moderately spicy but easy on the stomach.<br/><br/>Fast forward to 2016 when, with the help of a 3rd generation descendant of the recipe holder, Funhouse Malatang was started in Sydney, Australia. Having expanded to three locations, Funhouse continues to share the authentic Chongqing flavours and soul-warming broths to its happy customers.";
-
-//   aboutSection.appendChild(heading);
-//   aboutSection.appendChild(copy);
-
-//   return aboutSection;
-// }
-
-function createMenu() {
-  const menuSection = document.createElement("section");
-  menuSection.classList.add("menu");
-
-  const heading = document.createElement("h2");
-  heading.textContent = "Our menu";
-
-  menuSection.appendChild(heading);
-
-  return menuSection;
+  return section;
 }
