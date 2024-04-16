e3fed307d26595d3f7bf578f2aaad1ea5d8ae3f9
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-16 16:47:16 +1000

    add call to action button
    this button is to enable users to see where the locations are

--- src/home.js
+++ src/home.js
@@ -20,9 +20,13 @@ function createHero() {
   const smallText = document.createElement("p");
   smallText.textContent =
     "Pick from a wide range of meat, vegetables, seafood and other delicious ingredients, then we'll cook it for you in your choice of soup (or no soup) bases";
+  const btn = document.createElement("button");
+  btn.classList.add("cta");
+  btn.textContent = "Visit us";
 
   grid.appendChild(bigText);
   grid.appendChild(smallText);
+  grid.appendChild(btn);
   heroSection.appendChild(createHeader());
   heroSection.appendChild(grid);
 
