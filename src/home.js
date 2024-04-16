b6e871a93f60b17a9507e9b7672116ffe610efd4
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-16 11:01:05 +1000

    comment out about section. redesign homepage and add wireframe in docs

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ export function createHomePage() {
   const menuSection = createMenu();
 
   mainElement.appendChild(heroSection);
-  mainElement.appendChild(aboutSection);
+  // mainElement.appendChild(aboutSection);
   mainElement.appendChild(menuSection);
 }
 
@@ -32,22 +32,22 @@ function createHero() {
   return heroSection;
 }
 
-function createAbout() {
-  const aboutSection = document.createElement("section");
-  aboutSection.classList.add("about");
+// function createAbout() {
+//   const aboutSection = document.createElement("section");
+//   aboutSection.classList.add("about");
 
-  const heading = document.createElement("h2");
-  heading.textContent = "Our story";
+//   const heading = document.createElement("h2");
+//   heading.textContent = "Our story";
 
-  const copy = document.createElement("p");
-  copy.innerHTML =
-    "In 1908, a secret soup recipe in Chongqing kickstarted the beginning of Chinese hotpot as we know it. The recipe combined more than a dozen of ingredients and spices, creating the perfect hotpot broth: slightly numbing, moderately spicy but easy on the stomach.<br/><br/>Fast forward to 2016 when, with the help of a 3rd generation descendant of the recipe holder, Funhouse Malatang was started in Sydney, Australia. Having expanded to three locations, Funhouse continues to share the authentic Chongqing flavours and soul-warming broths to its happy customers.";
+//   const copy = document.createElement("p");
+//   copy.innerHTML =
+//     "In 1908, a secret soup recipe in Chongqing kickstarted the beginning of Chinese hotpot as we know it. The recipe combined more than a dozen of ingredients and spices, creating the perfect hotpot broth: slightly numbing, moderately spicy but easy on the stomach.<br/><br/>Fast forward to 2016 when, with the help of a 3rd generation descendant of the recipe holder, Funhouse Malatang was started in Sydney, Australia. Having expanded to three locations, Funhouse continues to share the authentic Chongqing flavours and soul-warming broths to its happy customers.";
 
-  aboutSection.appendChild(heading);
-  aboutSection.appendChild(copy);
+//   aboutSection.appendChild(heading);
+//   aboutSection.appendChild(copy);
 
-  return aboutSection;
-}
+//   return aboutSection;
+// }
 
 function createMenu() {
   const menuSection = document.createElement("section");
