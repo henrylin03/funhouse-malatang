bb19442693eb8f011d23063f40afd5713884236e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-17 14:51:00 +1000

    add copy
    used entity name per ABN as the copyright owner

--- src/footer.js
+++ src/footer.js
@@ -1,6 +1,11 @@
 export function createFooter() {
+  const COPYRIGHT_TEXT = "© 2024 LYW Catering Pty Ltd";
+
   const section = document.createElement("footer");
-  section.textContent = "test footer";
+  const text = document.createElement("small");
+  text.textContent = COPYRIGHT_TEXT;
+
+  section.appendChild(text);
 
   return section;
 }
