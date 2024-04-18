2164cd531631ec3642e51bd31c71f5e2087bbeaa
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-18 16:10:31 +1000

    update copy

--- src/menu.js
+++ src/menu.js
@@ -10,7 +10,7 @@ export function createMenuPage() {
   pageHeader.textContent = "Menu";
   const pageSubtext = document.createElement("p");
   pageSubtext.textContent =
-    "We ensure we have the highest quality ingredients available at all times for you to pick, all served in authentic, Chongqing-style, hotpot soup or dry malatang of your choice";
+    "We ensure to have the highest quality ingredients available at all times for you to pick, all served in authentic, Chongqing-style, hotpot soup or dry malatang of your choice.";
   innerContainer.appendChild(pageHeader);
   innerContainer.appendChild(pageSubtext);
   aboveTheFoldSection.appendChild(innerContainer);
