3c0856568f555291c45b4505f9ac16de67f40456
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-18 16:06:13 +1000

    create inner container for above the fold so there's no disconnect in background colour

--- src/menu.js
+++ src/menu.js
@@ -3,13 +3,17 @@ export function createMenuPage() {
 
   const aboveTheFoldSection = document.createElement("section");
   aboveTheFoldSection.classList.add("above-the-fold");
+  const innerContainer = document.createElement("div");
+  innerContainer.classList.add("inner");
+
   const pageHeader = document.createElement("h1");
   pageHeader.textContent = "Menu";
   const pageSubtext = document.createElement("p");
   pageSubtext.textContent =
     "We ensure we have the highest quality ingredients available at all times for you to pick, all served in authentic, Chongqing-style, hotpot soup or dry malatang of your choice";
-  aboveTheFoldSection.appendChild(pageHeader);
-  aboveTheFoldSection.appendChild(pageSubtext);
+  innerContainer.appendChild(pageHeader);
+  innerContainer.appendChild(pageSubtext);
+  aboveTheFoldSection.appendChild(innerContainer);
 
   mainElement.appendChild(aboveTheFoldSection);
 
