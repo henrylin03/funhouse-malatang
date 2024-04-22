d3da0d8d82f2195ec851b680886969cbbe11a7d0
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-22 13:26:43 +1000

    add event listeners

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import "./styles/style.css";
 import { createHomePage } from "./home";
 import { createMenuPage } from "./menu";
 
-createHomePage();
-createMenuPage();
+const page = (function () {
+  const homeBtn = document.querySelector("#home-btn");
+  const menuBtn = document.querySelector("#menu-btn");
+
+  // initialise home page
+  createHomePage();
+
+  // add event listeners
+  homeBtn.addEventListener("click", createHomePage);
+  menuBtn.addEventListener("click", createMenuPage);
+})();
