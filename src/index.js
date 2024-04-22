53f7f3eb0af25b711223bcf256852f4f2eef3970
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-22 13:29:29 +1000

    clicking logo creates home page
    used event listener here

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { createMenuPage } from "./menu";
 const page = (function () {
   const homeBtn = document.querySelector("#home-btn");
   const menuBtn = document.querySelector("#menu-btn");
+  const logo = document.querySelector(".logo");
 
   // initialise home page
   createHomePage();
 
   // add event listeners
+  logo.addEventListener("click", createHomePage);
   homeBtn.addEventListener("click", createHomePage);
   menuBtn.addEventListener("click", createMenuPage);
 })();
