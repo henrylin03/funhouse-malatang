35929ab9a6c76b1a2d140e35e7699c724e0f06eb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-24 11:40:12 +1000

    configure common js webpack config file

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/assets/images/logo.jpg",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
